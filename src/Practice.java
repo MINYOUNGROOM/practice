4a712bd1421e49b41e748801a38ac488114212ac
김민영 <kmyoffice99@gmail.com>
2025-04-25 13:21:53 +0900

    문장 영문 알파벳 별 출현 빈도 프로그램

--- src/Practice.java
+++ src/Practice.java
@@ -1,50 +1,66 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 /*
- * 김민영 Java 깜짝과제 1
+ * 김민영 Java 깜짝과제 2
  *
- *수강관리 프로그램 작성
+ * 문장 영문 알파벳 별 출현 빈도 프로그램 작성
+ *
+ * 문장의 각 영문 알파벳 별 출현 빈도의 개수,
+ * 전체 알파벳 중 몇 % 비율로 나오는지 구해라
  * */
-public class Practice {
-    Practice() {
-        System.out.println("<<<<[메뉴선택]>>>>");
-        System.out.println("1. 회원 관리");
-        System.out.println("2. 과목 관리");
-        System.out.println("3. 수강 관리");
-        System.out.println("4. 결제 관리");
-        System.out.println("5. 종료");
-    }
+class FileUtils {
 
-    public static void main(String[] args) {
-        new Practice();
+    public String getLoadText(String filePath) {
+        StringBuilder sb = new StringBuilder();
 
-        Scanner scanner = new Scanner(System.in);
-        int i;
-
-
-        while (true) {
-            i = scanner.nextInt();
-            switch (i) {
-                case 1:
-                    System.out.println("회원 관리 메뉴를 선택했습니다.");
-                    break;
-                case 2:
-                    System.out.println("과목 관리 메뉴를 선택했습니다.");
-                    break;
-                case 3:
-                    System.out.println("수강 관리 메뉴를 선택했습니다.");
-                    break;
-                case 4:
-                    System.out.println("결제 관리 메뉴를 선택했습니다.");
-                    new Practice();
-                    break;
-                case 5:
-                    System.out.println("프로그램을 종료합니다.");
-                    scanner.close();
-                    return; // while문 종료
-                default:
-                    System.out.println("입력값이 정확하지 않습니다.");
+        try {
+            Path path = Paths.get(filePath);
+            List<String> lines = Files.readAllLines(path);
+            for (int i = 0; i < lines.size(); i++) {
+                if (i > 0) {
+                    sb.append("\n");
+                }
+                sb.append(lines.get(i));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}
+
+
+public class Practice {
+    public static void main(String[] args) {
+        FileUtils fileUtils = new FileUtils();
+        String text = fileUtils.getLoadText("C:\\Users\\KIMMINYOUNG\\OneDrive\\바탕 화면\\Text.txt");
+        // 읽어온 파일을 전부 대문자로 변경
+        text = text.toUpperCase();
+        // 알파벳 별로 담길 배열 생성
+        int[] counts = new int[26];
+        // 전체 카운트 변수 정의
+        int totalCount = 0;
+
+        // 알파벳 별 카운트
+        for (int i = 0; i < text.length(); i++) {
+            char j = text.charAt(i);
+            if (j >= 'A' && j <= 'Z') {
+                counts[j - 'A']++;
+                totalCount++;
+            }
+        }
+
+        // 출력
+        for (int i = 0; i < 26; i++) {
+            char alpabet = (char) (i + 'A'); // i + 65
+            int count = counts[i];
+            double percentage = (count * 100.0) / totalCount;
+            String percentageFormat = String.format("%.2f", percentage);
+            System.out.println(alpabet + "     =" + count + "개,     " + percentageFormat + "%");
         }
     }
 }
